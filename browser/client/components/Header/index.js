9e546977f39296d846f965668456f46a1ecc0c30
sandnuggah <aaa@hueueueueu.eu>
2016-10-02 19:21:11 +0200

    Remove console.log

--- browser/client/components/Header/index.js
+++ browser/client/components/Header/index.js
@@ -3,15 +3,9 @@ import style from './style.css'
 
 class Header extends Component {
   render () {
-    console.log(this.props)
-
     return (
       <header className={style['normal']}>
         <h1>Megafon</h1>
-
-        <span>
-
-        </span>
       </header>
     )
   }
