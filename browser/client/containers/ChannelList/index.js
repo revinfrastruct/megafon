b696d92f5c0fdd39285d7076c8e016b166afb914
sandnuggah <adam@Adams-MacBook-Pro.local>
2016-10-15 16:10:24 +0200

    Get channels from props

--- browser/client/containers/ChannelList/index.js
+++ browser/client/containers/ChannelList/index.js
@@ -4,23 +4,9 @@ import {bindActionCreators} from 'redux'
 import * as actions from 'actions'
 import Channel from 'components/Channel'
 
-const data = [
-  {
-    topic: 'nmr',
-    title: 'NMR 12e September',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-  },
-
-  {
-    topic: 'forbifart-sthlm',
-    title: 'Förbifart Stockholm',
-    description: 'Consectetur adipisicing elit, sed do eiusmod tempor.'
-  }
-]
-
 class ChannelList extends Component {
   render () {
-    const channels = data.map((channel, index) => {
+    const channels = this.props.channels.map((channel, index) => {
       return <Channel key={index} {...channel} />
     })
 
