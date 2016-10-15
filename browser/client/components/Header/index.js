bd85e86468a055eabdfd47297d67cd57c97f6902
sandnuggah <adam@Adams-MacBook-Pro.local>
2016-10-15 16:22:55 +0200

    Makes title link back to root

--- browser/client/components/Header/index.js
+++ browser/client/components/Header/index.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router'
 import style from './style.css'
 
 class Header extends Component {
   render () {
     return (
       <header className={style['normal']}>
-        <h1>Megafon</h1>
+        <h1>
+          <Link to='/'>Megafon</Link>
+        </h1>
       </header>
     )
   }
