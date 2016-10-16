36e513f8cbb94db7ebf09612a42826589bf8c758
sandnuggah <adam@Adams-MacBook-Pro.local>
2016-10-16 23:50:11 +0200

    Change events data structure

--- browser/client/reducers/events.js
+++ browser/client/reducers/events.js
@@ -1,7 +1,10 @@
 import {handleActions} from 'redux-actions'
 import {ADD_EVENT} from 'constants'
 
-const initialState = []
+const initialState = {
+  channelFilter: null,
+  bucket: []
+}
 
 export default handleActions({
   [ADD_EVENT] (state, action) {
