880a4846795681cdab028d0c00567d27168e047a
sandnuggah <adam@Adams-MacBook-Pro.local>
2016-10-16 23:52:16 +0200

    Add addChannel action

--- browser/client/actions/index.js
+++ browser/client/actions/index.js
@@ -1,16 +1,21 @@
-import {ADD_EVENT} from 'constants'
+import {ADD_CHANNEL, ADD_EVENT, SET_CHANNEL_FILTER} from 'constants'
 
-export function startListeningToEvents () {
+export function addChannel (channel) {
   return (dispatch, getState) => {
-
+    dispatch({
+      type: ADD_CHANNEL, channel
+    })
   }
 }
 
 export function addEvent (event) {
   return (dispatch, getState) => {
     dispatch({
-      type: ADD_EVENT,
-      payload: event
+      type: ADD_EVENT, event
+    })
+  }
+}
+
 export function setChannelFilter (channelFilter) {
   return (dispatch, getState) => {
     dispatch({
