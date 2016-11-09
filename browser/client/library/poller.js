b58f6ca85a2a11efde1870faa03edee83f2e4b6a
0x4d <adam@Adams-MacBook-Pro.local>
2016-11-09 01:27:50 +0100

    Add Poller class

---
+++ browser/client/library/poller.js
@@ -0,0 +1,42 @@
+import 'whatwg-fetch'
+
+export default class Poller {
+  constructor () {
+    this.intervals = []
+  }
+
+  add (channel) {
+    this.intervals = [...this.intervals, {
+      channel,
+      interval: setInterval(this.fetchEvents, 5000, channel)
+    }]
+  }
+
+  remove (channel) {
+    const interval = this.intervals.find(interval => {
+      return interval.channel === channel
+    })
+
+    if (!interval) {
+      throw new Error(`Interval channel:${channel} not found!`)
+    }
+
+    clearInterval(interval.interval)
+
+    const idx = this.intervals.indexOf(interval)
+    this.intervals = [
+      ...this.intervals.splice(0, idx),
+      ...this.intervals.slice(idx + 1)
+    ]
+  }
+
+  fetchEvents (channel) {
+    const url = `https://modkraft.s3.amazonaws.com/tickers/data/${channel}.json`
+
+    fetch(url).then(response => response.json()).then(json => {
+      for (let key in json) {
+        console.log(json[key])
+      }
+    })
+  }
+}
