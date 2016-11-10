4859e3fc576332fb829aea073813c667af2cf335
0x4d <0x4d@bakunin.local>
2016-11-10 13:51:46 +0100

    Remove toggle component import

--- browser/client/containers/EventList/index.js
+++ browser/client/containers/EventList/index.js
@@ -3,7 +3,6 @@ import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import * as actions from 'actions'
 import Poller from 'library/poller'
-import Toggle from 'react-toggle'
 import Event from 'components/Event'
 import style from './style.css'
 require('react-toggle/style.css')
