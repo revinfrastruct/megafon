d1c465faac4591188f93710de0d4055e09086765
0x4d <0x4d@bakunin.local>
2016-11-10 09:56:25 +0100

    Remove title and unused icons

--- browser/client/components/Event/index.js
+++ browser/client/components/Event/index.js
@@ -2,29 +2,16 @@ import React from 'react'
 import style from './style.css'
 import moment from 'moment'
 import ReactHtmlParser from 'react-html-parser'
+import {IoSocialTwitter as Twitter} from 'react-icons/lib/io'
 
-import {
-  IoIosInformatoutline,
-  IoIosBellOutline,
-  IoSocialTwitter
-} from 'react-icons/lib/io'
-
-const icons = {
-  info: <IoIosInformatoutline size={35} color='#0000ff' />,
-  bell: <IoIosBellOutline size={35} color='#ff0000' />,
-  tweet: <IoSocialTwitter size={35} color='#1da1f2' />
-}
-
-const Event = ({kind, title, description, createdAt}) => {
+const Event = ({kind, description, createdAt}) => {
   return (
     <li className={style['normal']}>
       <div className={style['icon']}>
-        {icons[kind]}
+        <Twitter size={35} color='#1da1f2' />
       </div>
 
       <div className={style['content']}>
-        <h2>{title}</h2>
-
         <time dateTime={createdAt}>
           Publicerad {moment(createdAt).fromNow()}
         </time>
