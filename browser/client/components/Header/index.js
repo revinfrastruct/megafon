fc096ce2730f525d5c3dac0891f7dbebeec7b2f8
0x4d <0x4d@bakunin.local>
2016-11-10 11:39:04 +0100

    Import motkraft logo

--- browser/client/components/Header/index.js
+++ browser/client/components/Header/index.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router'
 import style from './style.css'
+import logoMotkraft from 'assets/motkraft.png'
 
 class Header extends Component {
   render () {
     return (
       <header className={style['normal']}>
+        <img src={logoMotkraft} alt='hello' />
+
         <h1>
           <Link to='/'>Megafon</Link>
         </h1>
