4d6d2a87c044251038eef098537a178c70d6dba4
alfred <alfred@nurd.nu>
2016-11-11 22:44:50 +0000

    use client's time zone

--- browser/client/components/Event/index.js
+++ browser/client/components/Event/index.js
@@ -3,11 +3,19 @@ import style from './style.css'
 import ReactHtmlParser from 'react-html-parser'
 
 const Event = ({kind, description, createdAt, media}) => {
+
+  const time = new Date(createdAt);
+  const timeStr = time.getUTCFullYear() + '-' +
+    ('00' + (time.getMonth()+1)).slice(-2) + '-' +
+    ('00' + time.getDate()).slice(-2) + ' ' + 
+    ('00' + time.getHours()).slice(-2) + ':' + 
+    ('00' + time.getMinutes()).slice(-2);
+
   return (
     <li className={style['normal']}>
       <div className={style['content']}>
         <time dateTime={createdAt}>
-          {(new Date(createdAt)).toISOString().slice(0, 19).replace('T', ' ').replace(/:[0-9]{2}$/, '')}
+          {(timeStr)}
         </time>
         {(
           media
